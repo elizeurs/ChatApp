d4ddc266d341de6f162b6f1500881d3f81c76fd3
Elizeu RS <13ders@gmail.com>
2023-07-18 18:42:22 -0300

    1. create the project

---
+++ ChatApp/ChatApp/Module/Controller/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  ChatApp
+//
+//  Created by Elizeu RS on 18/07/23.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+  //MARK: - Properties
+  
+  //MARK: - Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemRed
+    title = "Login"
+  }
+  
+  //MARK: - Helpers
+}
---
+++ ChatApp/ChatApp/Module/Controller/RegisterViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RegisterViewController.swift
+//  ChatApp
+//
+//  Created by Elizeu RS on 18/07/23.
+//
+
+import Foundation
+import UIKit
+
+class RegisterViewController: UIViewController {
+  //MARK: - Properties
+  
+  //MARK: - Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemBlue
+  }
+  
+  //MARK: - Helpers
+}
